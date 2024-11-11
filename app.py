d3d8100471282250014e1a65ed5838b2bfa97013
Frodnar <61243109+Frodnar@users.noreply.github.com>
2024-11-11 10:25:18 -0600

    Handle bugs and add examples.

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ from langchain_community.graphs import Neo4jGraph
 
 st.title("GDS Docs Bot")
 
+st.markdown("""Ask questions about Neo4j's Graph Data Science library via this bot!""")
+
+st.markdown("""It is backed by an AuraDB instance where I've loaded the [documentation](https://neo4j.com/docs/graph-data-science/2.12/) for the most recent full GDS release.  The page data was cloned from the [Github repo](https://github.com/neo4j/graph-data-science/tree/master/doc/modules/ROOT/pages).  The data were then [transformed into a knowledge graph](https://python.langchain.com/docs/how_to/graph_constructing/) as well as [embedded using OpenAI models](https://neo4j.com/developer-blog/neo4j-langchain-vector-index-implementation/) with both Cypher generation and embedding modes being used at query time.""")
+
+st.markdown("""Try asking questions such as:
+- *Return algorithms that are related to Breadth First Search?*
+- *What are the production quality centrality algorithms?*""")
+
 os.environ['OPENAI_API_KEY'] = st.secrets['api_keys']['openai']
 
 url=st.secrets['neo4j_info']['uri']
@@ -52,7 +60,7 @@ if query := st.chat_input("Ask a question about GDS."):
         try:
             graph_result = chain.invoke(query)
         except:
-            graph_result = "Generated Cypher query was invalid.  No result returned."
+            graph_result = "Cypher query returned no results."
         vector_result = existing_index_return.similarity_search(query, k=1)[0]
         
         final_prompt = f"""You are a helpful question-answering agent. Your task is to analyze
@@ -73,6 +81,15 @@ if query := st.chat_input("Ask a question about GDS."):
             ],
             stream=True,
         )
+        st.markdown("*Returned vector data:*")
+        vector_response = st.markdown("> " + vector_result.page_content[:300].replace("\n", "\n> ") + "...")
+        st.markdown("*Returned KG data:*" )
+        try:
+            graph_response = st.markdown("> " + graph_result['result'].replace("\n", "\n> "))
+        except:
+            graph_response = st.markdown("> " + graph_result[:300].replace("\n", "\n> "))
+        st.text("")
+        st.markdown("***Final response:***")
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
 
